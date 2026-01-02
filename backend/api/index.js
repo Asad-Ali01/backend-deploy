4605653ddaf07183c14ea59ebd85d1a63f4be526
unknown <https://github.com/Asad-Ali01/User-Managemenet-dashboard.git>
2026-01-02 21:52:02 +0500

    New changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/testing',(req,res) => {
-  res.json({myobj:"Here is testing answer"});
+  res.json({data:"Testing is solved"});
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
