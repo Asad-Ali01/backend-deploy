f96ba30b25a31fc6d1151b588b6ee2c9abf6b049
unknown <https://github.com/Asad-Ali01/User-Managemenet-dashboard.git>
2026-01-03 11:57:02 +0500

    Update backend with CORS support

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv');
 const app = express()
 const port = 3000
 dotenv.config()
+
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
